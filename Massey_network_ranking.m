8bbef6613bb9c6e0e132128541e59593b7e1d0db
Shiyu Tu <36581453+www6834@users.noreply.github.com>
2018-04-18 16:39:59 -0700

    Update Massey_network_ranking.m

--- Massey_network_ranking.m
+++ Massey_network_ranking.m
@@ -17,82 +17,41 @@ for i = 1:7
 end
 
 c = 1;
-b = 2;
 X = zeros(21,7);
 Y = zeros(21,1);
 for a = 1:7
-    for b = a+1:7
-        %while b <= 7
-            if Q(a) < Q(b)
-                X(c,a) = -1;
-                X(c,b) = 1;
-                c = c + 1;
-                %b = b + 1;
-            elseif Q(a) > Q(b)
-                X(c,b) = -1;
-                X(c,a) = 1;
-                c = c + 1;
-                %b = b + 1;
-            else 
-                X(c,b) = 0;
-                X(c,a) = 0;
-                c = c+1;
-            end
-        %end
-    end
-    
+    for b = (a + 1):7
+        if Q(a) < Q(b)
+            X(c,a) = -1;
+            X(c,b) = 1;
+            Y(c,1) = Q(b) - Q(a);
+            c = c + 1;
+        elseif Q(a) > Q(b)
+            X(c,b) = -1;
+            X(c,a) = 1;
+            Y(c,1) = Q(a) - Q(b);
+            c = c + 1;
+        elseif Q(a) == Q(b)
+            X(c,a) = 0;
+            X(c,b) = 0;
+            Y(c,1) = 0;
+            c = c + 1;
+        end
+    end    
 end
-
-disp(X);
-
-%for a = 1:7
-    %b = a+1
-    %c = 1
-    %for b <= 7
-        %if Q(a) < Q(b)
-         %   X(c,a) = -1;
-          %  X(c,b) = 1;
-           % c = c + 1;
-           % b = b + 1;
-        %elseif Q(1,a) > Q(1,b)    
-         %   X(c,a) = 1;
-          %  X(c,b) = -1;
-           % c = c + 1;
-            %b = b + 1
-        %end
-    %end
-%end
-%diap(X);
 % get the Massey Matrix with diagonal is the total games they played
 % and the others is the -1* (#question i played against j)
-%M = zeros(7,7);
-%for a = 1:7
- %   for b = 1:7
-  %      M(a,b) = -31;
-   %     M(a,a) = 6*31;
-   % end
-%end
+tans_X = X';
+M = tans_X * X;
+D = tans_X * Y;
 
-% get d from students' point differentials
-%D = zeros(7,1);
+r = M \ D;
+[sorted_value,ranked] = sort(r,'ascend');
+fprintf('\n');
 
-%for c = 1:7
- %      D(c,1) = D(c,1) + S(d,c);
-  %  end
-%end
+fprintf('The unique solution of question 1 to 7: \n')
+disp(r);
 
-% since M is not a full rank, we replace the last element with 1
-% and replace the last element of D with 0
-%M(7,:) = 1;
-%D(7) = 0;
-
-%r = M \ D;
-%[sorted_value,ranked] = sort(r,'ascend');
-%fprintf('\n');
-
-%fprintf('The unique solution of question 1 to 7: \n')
-%disp(r);
-
-%fprintf('The difficulty rank of these 7 questions is %d %d %d %d %d %d %d. \n', ranked(1), ranked(2), ranked(3), ranked(4), ranked(5), ranked(6), ranked(7))
-%fprintf('Seventh question is the most difficult and first question is the easiest.')
+fprintf('The difficulty rank of these 7 questions is %d %d %d %d %d %d %d. \n', ranked(1), ranked(2), ranked(3), ranked(4), ranked(5), ranked(6), ranked(7))
+fprintf('fifth and sixth question is the most difficult and first question is the easiest.')
 fprintf('\n')
